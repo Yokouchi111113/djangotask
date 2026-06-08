e605899a44e5a6c164f2138d6ccca57ca1753300
Ely111113 <kayaya1251@gmail.com>
2026-06-08 21:50:17 +0900

    6/8

--- config/task/tests.py
+++ config/task/tests.py
@@ -1,6 +1,7 @@
 import pytest
 from rest_framework.test import APIClient
 from accounts.models import CustomUser
+from task.models import Task
 
 # 認証なし
 def test_task_create_requires_authentication():
@@ -74,7 +75,7 @@ def test_signin_success():
     assert "access" in res.data
     assert "refresh" in res.data
 
-#　task作成
+#　認証あり・task作成・DBの状態確認
 @pytest.mark.django_db
 def test_authenticated_user_can_create_task():
     client = APIClient()
@@ -110,3 +111,79 @@ def test_authenticated_user_can_create_task():
     )
 
     assert res.status_code == 201
+
+    assert Task.objects.count() == 1
+    task = Task.objects.first()
+    assert res.data["title"] == "DRF勉強"
+    assert res.data["status"] == "doing"
+    assert task.user == user
+
+
+
+@pytest.fixture
+def user(db):
+    return CustomUser.objects.create_user(
+        email="test@example.com",
+        password="password123",
+        display_name="test"
+    )
+
+@pytest.fixture
+def user_b(db):
+    return CustomUser.objects.create_user(
+        email="testb@example.com",
+        password="password147",
+        display_name="test01"
+    )
+
+#タスク作成
+@pytest.fixture
+def task_factory():
+    def _task(user):
+        return Task.objects.create(
+            title="DRF",
+            status="doing",
+            user=user
+        )
+    return _task
+
+
+# 認証済みclientをuser指定で作る
+@pytest.fixture
+def auth_client():
+    def _auth_client(user, password):
+        client = APIClient()
+
+        res = client.post(
+            "/api/signin/",
+            {
+                "email": user.email,
+                "password": password
+            },
+            format="json"
+        )
+
+        client.credentials(
+            HTTP_AUTHORIZATION=f"Bearer {res.data['access']}"
+        )
+
+        return client
+    
+    return _auth_client
+
+
+def test_user_cannot_access_other_users_task(
+    auth_client,
+    user,
+    user_b,
+    task_factory,
+):
+    task_obj = task_factory(user)
+
+    client = auth_client(user_b, "password147")
+
+    res = client.get(
+        f"/api/tasks/{task_obj.id}/"
+    )
+
+    assert res.status_code == 404
