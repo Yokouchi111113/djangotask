2c08cedaa4dc26ec361d21b9381a4ac958e633d8
Ely111113 <kayaya1251@gmail.com>
2026-07-02 14:56:29 +0900

    7/2

--- config/tests/task-create.spec.js
+++ config/tests/task-create.spec.js
@@ -10,6 +10,59 @@ test('認証済みユーザーがタスクを作成できる', async ({ page }, 
   const email = `test-${testInfo.project.name}-${Date.now()}@example.com`;
   await signup(page, email, DEFAULT_PASSWORD);
   await signin(page, email, DEFAULT_PASSWORD);
+  await expect(page).toHaveURL('http://127.0.0.1:8000/tasks/');
   await createTask(page, title);
   await expect(page.getByText(title)).toBeVisible();
+});
+
+test('タイトルが100文字でもタスクを作成できる', async ({ page }, testInfo) => {
+  const email = `test-${testInfo.project.name}-${Date.now()}@example.com`;
+  await signup(page, email, DEFAULT_PASSWORD);
+  await signin(page, email, DEFAULT_PASSWORD);
+  await expect(page).toHaveURL('http://127.0.0.1:8000/tasks/');
+  const longTitle = "a".repeat(100);
+  await createTask(page, longTitle);
+  await expect(page.getByText(longTitle)).toBeVisible();
+});
+
+
+test('タイトル未入力ではタスクを作成できない', async ({ page }, testInfo) => {
+  const email = `test-${testInfo.project.name}-${Date.now()}@example.com`;
+  await signup(page, email, DEFAULT_PASSWORD);
+  await signin(page, email, DEFAULT_PASSWORD);
+  await expect(page).toHaveURL('http://127.0.0.1:8000/tasks/');
+  await createTask(page, '');
+  const valid = await page.locator("#title").evaluate(
+  el => el.checkValidity()
+  );
+  expect(valid).toBe(false);
+});
+
+test('タイトルは2文字以下では作成できない', async ({ page }, testInfo) => {
+  const email = `test-${testInfo.project.name}-${Date.now()}@example.com`;
+  await signup(page, email, DEFAULT_PASSWORD);
+  await signin(page, email, DEFAULT_PASSWORD);
+  await expect(page).toHaveURL('http://127.0.0.1:8000/tasks/');
+  await createTask(page, '12');
+  await expect(page.getByText('この項目は少なくとも3文字以上にしてください')).toBeVisible();
+});
+
+test('過去の日付ではタスクを作成できない', async ({ page }, testInfo) => {
+  const email = `test-${testInfo.project.name}-${Date.now()}@example.com`;
+  await signup(page, email, DEFAULT_PASSWORD);
+  await signin(page, email, DEFAULT_PASSWORD);
+  await expect(page).toHaveURL('http://127.0.0.1:8000/tasks/');
+  await createTask(page, title, "2026-06-11");
+  await expect(page.getByText('過去の日付は設定できません')).toBeVisible();
+});
+
+
+test('タイトルが100文字を超えるとタスクを作成できない', async ({ page }, testInfo) => {
+  const email = `test-${testInfo.project.name}-${Date.now()}@example.com`;
+  await signup(page, email, DEFAULT_PASSWORD);
+  await signin(page, email, DEFAULT_PASSWORD);
+  await expect(page).toHaveURL('http://127.0.0.1:8000/tasks/');
+  const longTitle = "a".repeat(101);
+  await createTask(page, longTitle);
+  await expect(page.getByText('この項目が100文字より長くならないようにしてください')).toBeVisible();
 });
--- config/tests/task-patch.spec.js
+++ config/tests/task-patch.spec.js
@@ -30,3 +30,49 @@ test('認証済みユーザーがタスクを編集できる', async ({ page }, 
   await expect(task.getByText('完了')).toBeVisible();
   await expect(task.getByText(updatedDueDate)).toBeVisible();
 });
+
+
+test('タイトルが２文字以下では編集できない', async ({ page }, testInfo) => {
+  const email = `test-${testInfo.project.name}-${Date.now()}@example.com`;
+  await signup(page, email, DEFAULT_PASSWORD);
+  await signin(page, email, DEFAULT_PASSWORD);
+  await expect(page.getByRole('link', { name: 'signout' })).toBeVisible();
+  await createTask(page, title);
+  await expect(page.getByText(title)).toBeVisible();
+  await page.getByRole('button', { name: '編集' }).click();
+  await page.getByRole('textbox', { name: 'タイトル' }).fill('65');
+  await page.getByRole('button', { name: '更新' }).click();
+  await expect(page.getByText('この項目は少なくとも3文字以上にしてください')).toBeVisible();
+});
+
+test('タイトルが100文字を超えるとタスクを編集できない', async ({ page }, testInfo) => {
+  const email = `test-${testInfo.project.name}-${Date.now()}@example.com`;
+  await signup(page, email, DEFAULT_PASSWORD);
+  await signin(page, email, DEFAULT_PASSWORD);
+  await expect(page.getByRole('link', { name: 'signout' })).toBeVisible();
+  await createTask(page, title);
+  await expect(page.getByText(title)).toBeVisible();
+  await page.getByRole('button', { name: '編集' }).click();
+  const longTitle = "a".repeat(101);
+  await page.getByRole('textbox', { name: 'タイトル' }).fill(longTitle);
+  await page.getByRole('button', { name: '更新' }).click();
+  await expect(page.getByText('この項目が100文字より長くならないようにしてください')).toBeVisible();
+});
+
+test('過去の日付ではタスクを編集できない', async ({ page }, testInfo) => {
+  const email = `test-${testInfo.project.name}-${Date.now()}@example.com`;
+  await signup(page, email, DEFAULT_PASSWORD);
+  await signin(page, email, DEFAULT_PASSWORD);
+  await expect(page.getByRole('link', { name: 'signout' })).toBeVisible();
+  await createTask(page, title);
+  await expect(page.getByText(title)).toBeVisible();
+  await page.getByRole('button', { name: '編集' }).click();
+  await page.getByRole('textbox', { name: 'タイトル' }).fill(updatedTitle);
+  await page.locator('#due_date').fill("2026-06-11");
+  await page.getByRole('button', { name: '更新' }).click();
+  await expect(page.getByText('過去の日付は設定できません')).toBeVisible();
+});
+
+
+
+
